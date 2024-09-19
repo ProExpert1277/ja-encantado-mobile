3235b6046fcd12921a224bb4aab1d75cce7a4de5
Mateus Roveda <mateus.roveda@universo.univates.br>
2024-09-19 17:04:33 -0300

    First commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,87 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, View, Text, Image, FlatList, TextInput, Button } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const DATA = [
+  {
+    id: '1', descricao: 'Maçã'
+  },
+  {
+    id: '2', descricao: 'Bola de futebol'
+  },
+  {
+    id: '3', descricao: 'Batata'
+  },
+  {
+    id: '4', descricao: 'Banana'
+  },
+]
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <Image
+        style={styles.capa}
+        source={require('@/assets/images/capa.jpg')}
+      />
+      <View style={styles.containerCabecalho}>
+        <Text style={styles.cabecalho}>Lista de compras</Text>
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          inputMode='text'
+          placeholder='Digite o item para adicionar a lista'
+          placeholderTextColor={'#428df5'}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <Button 
+          title="Adicionar"
+          color="#428df5"
+        />
+      </View>
+      <FlatList
+        style={styles.containerLista}
+        data={DATA}
+        renderItem={({ item }) => (
+          <View>
+            <Text style={styles.textoLista}>• {item.id} - {item.descricao}</Text>
+          </View>
+        )}
+        keyExtractor={item => item.id}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
+    backgroundColor: '#3c3c3c',
+    justifyContent: 'flex-start',
+    marginTop: 30
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  containerCabecalho: {
+    alignItems: 'center'
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  capa: {
+    width: '100%',
+    height: 200
+  },
+  cabecalho: {
+    color: '#FFF',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  textoLista: {
+    color: '#FFF',
+    fontSize: 15
+  },
+  containerLista: {
+    padding: 20
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    color: '#FFF'
   },
 });
